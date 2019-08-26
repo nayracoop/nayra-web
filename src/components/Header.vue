4b6c190a4acb76b11342f03261094b44db42f9b4
Tobias <tirigoyen@nayra.com.ar>
2019-08-26 13:32:56 -0300

    Components creation

---
+++ src/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+<div></div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  },
+  methods: {
+    setLocale (locale) {
+      import(`../translations/${locale}.json`).then(msgs => {
+        this.$i18n.setLocaleMessage(locale, msgs)
+        this.$i18n.locale = locale
+      })
+    }
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+</style>
