6fe30e99b3c72309076f4a7e23097dd83066a883
Tobias <tirigoyen@nayra.com.ar>
2019-08-27 13:55:08 -0300

    Portfolio icons

--- src/components/Portfolio.vue
+++ src/components/Portfolio.vue
@@ -7,38 +7,8 @@
         <li>
           <input type="checkbox" id="web" name="web" class="sr-only" value="web" />
           <label for="web" class="btn btn-secondary">
-            <svg
-              class="svg-inline--fa fa-check-square fa-w-14"
-              aria-hidden="true"
-              data-fa-processed
-              data-prefix="far"
-              data-icon="check-square"
-              role="img"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 448 512"
-            >
-              <path
-                fill="currentColor"
-                d="M400 32H48C21.49 32 0 53.49 0 80v352c0 26.51 21.49 48 48 48h352c26.51 0 48-21.49 48-48V80c0-26.51-21.49-48-48-48zm0 400H48V80h352v352zm-35.864-241.724L191.547 361.48c-4.705 4.667-12.303 4.637-16.97-.068l-90.781-91.516c-4.667-4.705-4.637-12.303.069-16.971l22.719-22.536c4.705-4.667 12.303-4.637 16.97.069l59.792 60.277 141.352-140.216c4.705-4.667 12.303-4.637 16.97.068l22.536 22.718c4.667 4.706 4.637 12.304-.068 16.971z"
-              />
-            </svg>
-            <!-- <i class="far fa-check-square"></i> -->
-            <svg
-              class="svg-inline--fa fa-square fa-w-14"
-              aria-hidden="true"
-              data-fa-processed
-              data-prefix="far"
-              data-icon="square"
-              role="img"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 448 512"
-            >
-              <path
-                fill="currentColor"
-                d="M400 32H48C21.5 32 0 53.5 0 80v352c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48V80c0-26.5-21.5-48-48-48zm-6 400H54c-3.3 0-6-2.7-6-6V86c0-3.3 2.7-6 6-6h340c3.3 0 6 2.7 6 6v340c0 3.3-2.7 6-6 6z"
-              />
-            </svg>
-            <!-- <i class="far fa-square"></i> -->
+            <font-awesome-icon :icon="['far', 'check-square']" />
+            <font-awesome-icon :icon="['far', 'square']" />
             Web
           </label>
         </li>
@@ -51,161 +21,40 @@
             value="e-commerce"
           />
           <label for="e-commerce" class="btn btn-secondary">
-            <svg
-              class="svg-inline--fa fa-check-square fa-w-14"
-              aria-hidden="true"
-              data-fa-processed
-              data-prefix="far"
-              data-icon="check-square"
-              role="img"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 448 512"
-            >
-              <path
-                fill="currentColor"
-                d="M400 32H48C21.49 32 0 53.49 0 80v352c0 26.51 21.49 48 48 48h352c26.51 0 48-21.49 48-48V80c0-26.51-21.49-48-48-48zm0 400H48V80h352v352zm-35.864-241.724L191.547 361.48c-4.705 4.667-12.303 4.637-16.97-.068l-90.781-91.516c-4.667-4.705-4.637-12.303.069-16.971l22.719-22.536c4.705-4.667 12.303-4.637 16.97.069l59.792 60.277 141.352-140.216c4.705-4.667 12.303-4.637 16.97.068l22.536 22.718c4.667 4.706 4.637 12.304-.068 16.971z"
-              />
-            </svg>
-            <!-- <i class="far fa-check-square"></i> -->
-            <svg
-              class="svg-inline--fa fa-square fa-w-14"
-              aria-hidden="true"
-              data-fa-processed
-              data-prefix="far"
-              data-icon="square"
-              role="img"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 448 512"
-            >
-              <path
-                fill="currentColor"
-                d="M400 32H48C21.5 32 0 53.5 0 80v352c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48V80c0-26.5-21.5-48-48-48zm-6 400H54c-3.3 0-6-2.7-6-6V86c0-3.3 2.7-6 6-6h340c3.3 0 6 2.7 6 6v340c0 3.3-2.7 6-6 6z"
-              />
-            </svg>
-            <!-- <i class="far fa-square"></i> -->
+            <font-awesome-icon :icon="['far', 'square']" />
+            <font-awesome-icon :icon="['far', 'check-square']" />
             E-commerce
           </label>
         </li>
         <li>
           <input type="checkbox" id="medios" name="medios" class="sr-only" value="medios" />
           <label for="medios" class="btn btn-secondary">
-            <svg
-              class="svg-inline--fa fa-check-square fa-w-14"
-              aria-hidden="true"
-              data-fa-processed
-              data-prefix="far"
-              data-icon="check-square"
-              role="img"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 448 512"
-            >
-              <path
-                fill="currentColor"
-                d="M400 32H48C21.49 32 0 53.49 0 80v352c0 26.51 21.49 48 48 48h352c26.51 0 48-21.49 48-48V80c0-26.51-21.49-48-48-48zm0 400H48V80h352v352zm-35.864-241.724L191.547 361.48c-4.705 4.667-12.303 4.637-16.97-.068l-90.781-91.516c-4.667-4.705-4.637-12.303.069-16.971l22.719-22.536c4.705-4.667 12.303-4.637 16.97.069l59.792 60.277 141.352-140.216c4.705-4.667 12.303-4.637 16.97.068l22.536 22.718c4.667 4.706 4.637 12.304-.068 16.971z"
-              />
-            </svg>
-            <!-- <i class="far fa-check-square"></i> -->
-            <svg
-              class="svg-inline--fa fa-square fa-w-14"
-              aria-hidden="true"
-              data-fa-processed
-              data-prefix="far"
-              data-icon="square"
-              role="img"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 448 512"
-            >
-              <path
-                fill="currentColor"
-                d="M400 32H48C21.5 32 0 53.5 0 80v352c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48V80c0-26.5-21.5-48-48-48zm-6 400H54c-3.3 0-6-2.7-6-6V86c0-3.3 2.7-6 6-6h340c3.3 0 6 2.7 6 6v340c0 3.3-2.7 6-6 6z"
-              />
-            </svg>
-            <!-- <i class="far fa-square"></i> -->
+            <font-awesome-icon :icon="['far', 'square']" />
+            <font-awesome-icon :icon="['far', 'check-square']" />
             Medios
           </label>
         </li>
         <li>
           <input type="checkbox" id="apps" name="apps" class="sr-only" value="apps" />
           <label for="apps" class="btn btn-secondary">
+            <font-awesome-icon :icon="['far', 'square']" />
             <font-awesome-icon :icon="['far', 'check-square']" />
-            
-            <!-- <i class="far fa-square"></i> -->
             Apps
           </label>
         </li>
         <li>
           <input type="checkbox" id="arte" name="arte" class="sr-only" value="arte" />
           <label for="arte" class="btn btn-secondary">
-            <svg
-              class="svg-inline--fa fa-check-square fa-w-14"
-              aria-hidden="true"
-              data-fa-processed
-              data-prefix="far"
-              data-icon="check-square"
-              role="img"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 448 512"
-            >
-              <path
-                fill="currentColor"
-                d="M400 32H48C21.49 32 0 53.49 0 80v352c0 26.51 21.49 48 48 48h352c26.51 0 48-21.49 48-48V80c0-26.51-21.49-48-48-48zm0 400H48V80h352v352zm-35.864-241.724L191.547 361.48c-4.705 4.667-12.303 4.637-16.97-.068l-90.781-91.516c-4.667-4.705-4.637-12.303.069-16.971l22.719-22.536c4.705-4.667 12.303-4.637 16.97.069l59.792 60.277 141.352-140.216c4.705-4.667 12.303-4.637 16.97.068l22.536 22.718c4.667 4.706 4.637 12.304-.068 16.971z"
-              />
-            </svg>
-            <!-- <i class="far fa-check-square"></i> -->
-            <svg
-              class="svg-inline--fa fa-square fa-w-14"
-              aria-hidden="true"
-              data-fa-processed
-              data-prefix="far"
-              data-icon="square"
-              role="img"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 448 512"
-            >
-              <path
-                fill="currentColor"
-                d="M400 32H48C21.5 32 0 53.5 0 80v352c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48V80c0-26.5-21.5-48-48-48zm-6 400H54c-3.3 0-6-2.7-6-6V86c0-3.3 2.7-6 6-6h340c3.3 0 6 2.7 6 6v340c0 3.3-2.7 6-6 6z"
-              />
-            </svg>
-            <!-- <i class="far fa-square"></i> -->
+            <font-awesome-icon :icon="['far', 'square']" />
+            <font-awesome-icon :icon="['far', 'check-square']" />
             Arte
           </label>
         </li>
         <li>
           <input type="checkbox" id="otros" name="otros" class="sr-only" value="otros" />
           <label for="otros" class="btn btn-secondary">
-            <svg
-              class="svg-inline--fa fa-check-square fa-w-14"
-              aria-hidden="true"
-              data-fa-processed
-              data-prefix="far"
-              data-icon="check-square"
-              role="img"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 448 512"
-            >
-              <path
-                fill="currentColor"
-                d="M400 32H48C21.49 32 0 53.49 0 80v352c0 26.51 21.49 48 48 48h352c26.51 0 48-21.49 48-48V80c0-26.51-21.49-48-48-48zm0 400H48V80h352v352zm-35.864-241.724L191.547 361.48c-4.705 4.667-12.303 4.637-16.97-.068l-90.781-91.516c-4.667-4.705-4.637-12.303.069-16.971l22.719-22.536c4.705-4.667 12.303-4.637 16.97.069l59.792 60.277 141.352-140.216c4.705-4.667 12.303-4.637 16.97.068l22.536 22.718c4.667 4.706 4.637 12.304-.068 16.971z"
-              />
-            </svg>
-            <!-- <i class="far fa-check-square"></i> -->
-            <svg
-              class="svg-inline--fa fa-square fa-w-14"
-              aria-hidden="true"
-              data-fa-processed
-              data-prefix="far"
-              data-icon="square"
-              role="img"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 448 512"
-            >
-              <path
-                fill="currentColor"
-                d="M400 32H48C21.5 32 0 53.5 0 80v352c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48V80c0-26.5-21.5-48-48-48zm-6 400H54c-3.3 0-6-2.7-6-6V86c0-3.3 2.7-6 6-6h340c3.3 0 6 2.7 6 6v340c0 3.3-2.7 6-6 6z"
-              />
-            </svg>
-            <!-- <i class="far fa-square"></i> -->
+            <font-awesome-icon :icon="['far', 'square']" />
+            <font-awesome-icon :icon="['far', 'check-square']" />
             Otros
           </label>
         </li>
@@ -242,20 +91,7 @@
                 <img src="img/portfolio/amg-3.jpg" alt="AMG" class="d-block w-100" />
               </a>
             </div>
-            <svg
-              aria-hidden="true"
-              data-prefix="fas"
-              data-icon="search-plus"
-              role="img"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 512 512"
-              class="svg-inline--fa fa-search-plus fa-w-16"
-            >
-              <path
-                fill="currentColor"
-                d="M304 192v32c0 6.6-5.4 12-12 12h-56v56c0 6.6-5.4 12-12 12h-32c-6.6 0-12-5.4-12-12v-56h-56c-6.6 0-12-5.4-12-12v-32c0-6.6 5.4-12 12-12h56v-56c0-6.6 5.4-12 12-12h32c6.6 0 12 5.4 12 12v56h56c6.6 0 12 5.4 12 12zm201 284.7L476.7 505c-9.4 9.4-24.6 9.4-33.9 0L343 405.3c-4.5-4.5-7-10.6-7-17V372c-35.3 27.6-79.7 44-128 44C93.1 416 0 322.9 0 208S93.1 0 208 0s208 93.1 208 208c0 48.3-16.4 92.7-44 128h16.3c6.4 0 12.5 2.5 17 7l99.7 99.7c9.3 9.4 9.3 24.6 0 34zM344 208c0-75.2-60.8-136-136-136S72 132.8 72 208s60.8 136 136 136 136-60.8 136-136z"
-              />
-            </svg>
+            <font-awesome-icon icon="search-plus" />
           </div>
           <a href="https://www.amg.com.ar/" target="_blank" rel="nofollow" class="info">
             <h3>AMG</h3>
@@ -264,22 +100,7 @@
       </div>
     </div>
     <a href="#company-info" class="to-top nav-link">
-      <svg
-        class="svg-inline--fa fa-arrow-up fa-w-14"
-        aria-hidden="true"
-        data-fa-processed
-        data-prefix="fas"
-        data-icon="arrow-up"
-        role="img"
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 448 512"
-      >
-        <path
-          fill="currentColor"
-          d="M34.9 289.5l-22.2-22.2c-9.4-9.4-9.4-24.6 0-33.9L207 39c9.4-9.4 24.6-9.4 33.9 0l194.3 194.3c9.4 9.4 9.4 24.6 0 33.9L413 289.4c-9.5 9.5-25 9.3-34.3-.4L264 168.6V456c0 13.3-10.7 24-24 24h-32c-13.3 0-24-10.7-24-24V168.6L69.2 289.1c-9.3 9.8-24.8 10-34.3.4z"
-        />
-      </svg>
-      <!-- <i class="fas fa-arrow-up"></i> -->
+      <font-awesome-icon icon="arrow-up" />
     </a>
   </section>
 </template>
